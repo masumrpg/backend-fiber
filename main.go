647f8046831102822d5e45e2e09b5870e76939b1
masumrpg <masumrpg@gmail.com>
2024-02-18 16:16:44 +0700

    fix Port

--- main.go
+++ main.go
@@ -9,25 +9,22 @@ import (
 )
 
 func main() {
-	// ==========================================================================================
 	config.SetupDatabase()
 
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-	// app.Get("/", func(c *fiber.Ctx) error { return c.JSON(fiber.Map{"message": "Runing..."}) })
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.SendString("Hello, World!")
+	// })
+	app.Get("/", func(c *fiber.Ctx) error { return c.JSON(fiber.Map{"message": "Runing..."}) })
 
 	// Route app
 	routes.SetupAuthRoutes(app)
 	routes.SetupUserRoutes(app)
 
-	// 	// Mengambil nilai PORT dari environment variable
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000" // Port default jika PORT tidak diset
+		port = "3000"
 	}
 
-	// Mendengarkan port yang disediakan oleh Railway
 	app.Listen(":" + port)
 }
